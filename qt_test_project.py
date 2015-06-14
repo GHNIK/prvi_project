8e648c0244cd862ff145ba37accd3531c5c2a5d1
Vladimir Jankijevic <v.jankijevic@gmail.com>
2015-06-14 13:02:07 -0400

    reverts to PyQt4 deletes PySide

--- qt_test_project.py
+++ qt_test_project.py
@@ -3,12 +3,9 @@
 import os
 import sys
 
-try:
-    from PySide import QtCore, QtGui
-    from PySide.QtCore import Qt
-except ImportError:
-    from PyQt4 import QtCore, QtGui
-    from PyQt4.QtCore import Qt
+
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import Qt
 
 
 
